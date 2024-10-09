e198905da28b4b3026afa88b7e4c753936a81db8
praveenng <75559018+wmpn@users.noreply.github.com>
2024-10-09 20:13:00 +0530

    Added base UI components

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,29 @@
-export default function Home() {
-  return <h1>Home</h1>;
+import Navbar from "@/components/base/Navbar";
+import HeroSection from "@/components/base/HeroSection";
+import FeatureSection from "@/components/base/FeatureSection";
+import UserReviews from "@/components/base/UserReviews";
+import Footer from "@/components/base/Footer";
+import { getServerSession } from "next-auth";
+import { authOptions, CustomSession } from "./api/auth/[...nextauth]/options";
+
+export default async function Home() {
+  const session: CustomSession | null = await getServerSession(authOptions);
+
+  return (
+    <div className="">
+      {/* Header */}
+      <Navbar user={} />
+      {/* Hero Section */}
+      <HeroSection />
+
+      {/* Features Section */}
+      <FeatureSection />
+
+      {/* User Reviews Section */}
+      <UserReviews />
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
 }
