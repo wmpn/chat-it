203f95b8cac30e30258e6b572ec4a365c383e3d5
praveenng <75559018+wmpn@users.noreply.github.com>
2024-10-09 23:55:26 +0530

    Adding server side login

---
+++ server/src/controllers/AuthController.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import prisma from "../config/db.config.js";
+import jwt from "jsonwebtoken";
+
+interface LoginPayloadType {
+  name: string;
+  email: string;
+  provider: string;
+  oauth_id: string;
+  image?: string;
+}
+
+class AuthController {
+  static async login(req: Request, res: Response) {
+    try {
+      const body: LoginPayloadType = req.body;
+      let findUser = await prisma.user.findUnique({
+        where: {
+          email: body.email,
+        },
+      });
+
+      if (!findUser) {
+        findUser = await prisma.user.create({
+          data: body,
+        });
+      }
+
+      let jwtPayload = {
+        name: body.name,
+        email: body.email,
+        id: findUser.id,
+      };
+
+      const token = jwt.sign(jwtPayload, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+
+      return res.status(200).json({ ...findUser, token: `Bearer ${token}` });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: "Something went wrong, Please try again" });
+    }
+  }
+}
+
+export default AuthController;
