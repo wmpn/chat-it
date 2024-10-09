cf6812b9ed380a244ce66ffc740c2f8c60949423
praveenng <75559018+wmpn@users.noreply.github.com>
2024-10-09 13:39:19 +0530

    Client side initial commit

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "ChatIt",
+  description: "Chat with your loved ones securely",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "flex min-h-screen flex-col items-center font-sans bg-background antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
